a910062367bed240ca299bd798e04422f11c55ed
Ralph Alberti <ralphalberti21@gmail.com>
2026-03-11 10:50:27 -0400

    added my custom option-overrides

---
+++ lua/custom/options.lua
@@ -0,0 +1,13 @@
+-- Ralph's Neovim options
+
+-- relative line numbers
+vim.o.relativenumber = true
+
+-- keep cursor centered
+vim.o.scrolloff = 5
+
+-- faster responsiveness
+vim.o.updatetime = 200
+
+-- better command completion
+vim.o.wildmode = 'longest:full,full'
