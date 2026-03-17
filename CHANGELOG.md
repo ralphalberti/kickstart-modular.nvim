adeeeb760231fd5ea847755d0527832239a2ae9b
Ralph Alberti <ralphalberti21@gmail.com>
2026-03-17 10:34:55 -0400

    refactor: move debug setup to custom/plugins

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,49 +2,54 @@
 
 All notable changes to this Neovim configuration will be documented in this file.
 
-## [0.4.0] - 2026-03-16
+The format loosely follows Keep a Changelog.
+
+---
+
+## [0.3.0] - 2026-03-16
 
 ### Added
 
-- feat: add python formatting with black via conform.nvim
-- docs: update changelog for python formatting feature
+- Python formatting with **Black** via `conform.nvim`
+- Markdown linting using `markdownlint`
+- Markdown lint configuration via `.markdownlint.json`
 
-## [0.3.0] - 2026-03-14
+---
 
-### Added
-
-- feat: enable python linting with nvim-lint and ruff
-- docs: update changelog for linting feature
-
-## [0.2.0] - 2026-03-13
+## [0.2.0] - 2026-03-14
 
 ### Added
 
-- Autopairs plugin moved to lua/custom/plugins and installed
-- Indent_line plugin moved to lua/custom/plugins and installed
-- Added this changelog
+- Python linting with `nvim-lint`
+- Ruff integration for fast Python linting
+- Initial `CHANGELOG.md`
 
-## [0.1.0] - 2026-03-12
+---
+
+## [0.1.0] - 2026-03-13
 
 ### Added
 
-- Custom plugin namespace (`lua/custom/plugins`)
 - Neo-tree file explorer
-- Telescope hidden-file search
+- Autopairs plugin
+- Indent line plugin
+- Custom plugin namespace (`lua/custom/plugins`)
 - Custom options override file
 - Custom keymap override system
 
 ### Changed
 
-- Telescope `<leader>sf` mapping to support hidden files
-- Telescope `<leader>sF` mapping to support ignored files
+- Telescope `<leader>sf` mapping updated to support hidden files
+- Telescope `<leader>sF` mapping updated to support ignored files
+
+---
 
 ## [bootstrap-working] - 2026-03-09
 
 ### Added
 
-- Initial kickstart-modular config
+- Initial `kickstart-modular.nvim` configuration
 
 ### Fixed
 
-- Fix treesitter config path after upstream breaking change
+- Treesitter configuration path issue during startup
---
+++ lua/custom/plugins/debug.lua
@@ -0,0 +1,108 @@
+return {
+  {
+    'mfussenegger/nvim-dap',
+    dependencies = {
+      'rcarriga/nvim-dap-ui',
+      'nvim-neotest/nvim-nio',
+      'mfussenegger/nvim-dap-python',
+    },
+    keys = {
+      {
+        '<F5>',
+        function()
+          require('dap').continue()
+        end,
+        desc = 'Debug: Start/Continue',
+      },
+      {
+        '<F1>',
+        function()
+          require('dap').step_into()
+        end,
+        desc = 'Debug: Step Into',
+      },
+      {
+        '<F2>',
+        function()
+          require('dap').step_over()
+        end,
+        desc = 'Debug: Step Over',
+      },
+      {
+        '<F3>',
+        function()
+          require('dap').step_out()
+        end,
+        desc = 'Debug: Step Out',
+      },
+      {
+        '<leader>b',
+        function()
+          require('dap').toggle_breakpoint()
+        end,
+        desc = 'Debug: Toggle Breakpoint',
+      },
+      {
+        '<leader>B',
+        function()
+          require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+        end,
+        desc = 'Debug: Set Breakpoint',
+      },
+      {
+        '<F7>',
+        function()
+          require('dapui').toggle()
+        end,
+        desc = 'Debug: Toggle UI',
+      },
+      {
+        '<leader>dt',
+        function()
+          require('dap-python').test_method()
+        end,
+        desc = 'Debug Python: Test Method',
+      },
+      {
+        '<leader>dT',
+        function()
+          require('dap-python').test_class()
+        end,
+        desc = 'Debug Python: Test Class',
+      },
+    },
+    config = function()
+      local dap = require 'dap'
+      local dapui = require 'dapui'
+
+      dapui.setup {
+        icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+        controls = {
+          icons = {
+            pause = '⏸',
+            play = '▶',
+            step_into = '⏎',
+            step_over = '⏭',
+            step_out = '⏮',
+            step_back = 'b',
+            run_last = '▶▶',
+            terminate = '⏹',
+            disconnect = '⏏',
+          },
+        },
+      }
+
+      require('dap-python').setup(vim.fn.expand '~/.virtualenvs/debugpy/bin/python')
+
+      dap.listeners.after.event_initialized['dapui_config'] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated['dapui_config'] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited['dapui_config'] = function()
+        dapui.close()
+      end
+    end,
+  },
+}
