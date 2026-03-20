9419df004da2085d9fc161fc3b3b73032e18c60f
Ralph Alberti <ralphalberti21@gmail.com>
2026-03-19 22:27:24 -0400

    Add markdownlint and stylua with format-on-save

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -3,6 +3,12 @@ return {
   opts = {
     formatters_by_ft = {
       python = { 'black' },
+      markdown = { 'markdownlint' },
+      lua = { 'stylua' },
+    },
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_format = 'fallback',
     },
   },
 }
